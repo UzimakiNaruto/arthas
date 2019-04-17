b1d7ce58cba19aa394df55890696f88f02058d33
yongwei.fan <yongwei.fan@dianrong.com>
2019-04-17 16:34:21 +0800

    asm

--- demo/src/main/test/demo/AsmTest.java
+++ demo/src/main/test/demo/AsmTest.java
@@ -4,8 +4,10 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassVisitor;
@@ -13,51 +15,67 @@ import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.FieldVisitor;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.util.TraceClassVisitor;
 
 public class AsmTest implements Opcodes {
 
   private static int age = 21;
   private String name = "abc";
 
+  private MyClassLoader loader = new MyClassLoader();
+
   public static String before() throws IOException {
     return "hello";
   }
 
   @Test
+  public void trace() throws IOException, NoSuchMethodException {
+    String newClassName = AsmTest.class.getName().replaceAll("\\.", "/") + "A";
+
+    ClassReader cr = new ClassReader(AsmTest.class.getName());
+    ClassWriter cw = new ClassWriter(0);
+    // TraceClassVisitor trace = new TraceClassVisitor(cw, new PrintWriter(System.out));
+    ClassVisitor cv = new ClassVisitor(ASM7, cw) {
+      @Override
+      public void visit(int version, int access, String name, String signature, String superName,
+          String[] interfaces) {
+        super.visit(version, access, newClassName, signature, superName, interfaces);
+      }
+
+      @Override
+      public MethodVisitor visitMethod(int access, String name, String descriptor, String signature,
+          String[] exceptions) {
+
+        MethodVisitor mv = super.visitMethod(access, name, descriptor, signature, exceptions);
+        // if ("before".equals(name)) {
+        //   return null;
+        // }
+
+        return mv;
+      }
+
+      @Override
+      public void visitEnd() {
+        super.visitEnd();
+      }
+    };
+
+    cr.accept(cv, 0);
+
+    byte[] bytes = cw.toByteArray();
+    Class<?> clazz = loader.defineClass(newClassName.replaceAll("/", "."), bytes);
+    try {
+      Method m = clazz.getDeclaredMethod("before");
+    } catch (NoSuchMethodException e) {
+      e.printStackTrace();
+    }
+
+  }
+
+  @Test
+  @Ignore
   public void asm()
       throws IOException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-    // ClassReader cr = new ClassReader(AsmTest.class.getName());
-    // cr.accept(new ClassVisitor(ASM7) {
-    //   @Override
-    //   public void visit(int version, int access, String name, String signature, String superName,
-    //       String[] interfaces) {
-    //     super.visit(version, access, name, signature, superName, interfaces);
-    //   }
-    //
-    //   @Override
-    //   public MethodVisitor visitMethod(int access, String name, String descriptor, String signature,
-    //       String[] exceptions) {
-    //
-    //     if ("before".equals(name)) {
-    //
-    //       MethodVisitor mv = super.visitMethod(access, name, descriptor, signature, exceptions);
-    //
-    //       if (mv != null) {
-    //         System.out.println("can control");
-    //       }
-    //       return mv;
-    //     }
-    //
-    //     return super.visitMethod(access, name, descriptor, signature, exceptions);
-    //   }
-    //
-    //   @Override
-    //   public void visitEnd() {
-    //
-    //     super.visitEnd();
-    //   }
-    // }, 0);
-
     ClassWriter cw = new ClassWriter(2);
     String name = "demo/F";
     cw.visit(V1_8, ACC_PUBLIC, name, null, "demo/AsmTest", null);
@@ -65,21 +83,22 @@ public class AsmTest implements Opcodes {
     FieldVisitor fv = cw.visitField(ACC_PUBLIC + ACC_STATIC, "age", "I", null, 3);
     fv.visitEnd();
 
-    MethodVisitor mv = cw.visitMethod(ACC_PUBLIC, "<clinit>", "()V", null, null);
-    mv.visitCode();
-    mv.visitInsn(ICONST_3);
-    mv.visitFieldInsn(PUTSTATIC, name, "age", "I");
-    mv.visitEnd();
-    cw.visitEnd();
+    // MethodVisitor mv = cw.visitMethod(ACC_PUBLIC, "<clinit>", "()V", null, null);
+    // mv.visitCode();
+    // mv.visitInsn(ICONST_3);
+    // mv.visitFieldInsn(PUTSTATIC, name, "age", "I");
+    // mv.visitEnd();
+    // cw.visitEnd();
 
     byte[] bytes = cw.toByteArray();
     writeToClass(bytes);
 
-    MyClassLoader loader = new MyClassLoader();
     Class<?> clazz = loader.defineClass(name.replaceAll("/", "."), bytes);
-    Method m = clazz.getDeclaredMethod("before");
+
+    Method m = clazz.getMethod("before");
     m.setAccessible(true);
     Object value = m.invoke(null);
+
     System.out.println(value);
   }
 
@@ -90,7 +109,7 @@ public class AsmTest implements Opcodes {
       boolean newFile = f.createNewFile();
     }
 
-    try(FileOutputStream out = new FileOutputStream(f)) {
+    try (FileOutputStream out = new FileOutputStream(f)) {
       out.write(bytes);
     }
   }
