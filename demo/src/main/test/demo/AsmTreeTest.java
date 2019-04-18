29383bff679f46cdfc90f0d22f530e662dcaefec
yongwei.fan <yongwei.fan@dianrong.com>
2019-04-18 17:35:59 +0800

    ClassNode#accept

--- demo/src/main/test/demo/AsmTreeTest.java
+++ demo/src/main/test/demo/AsmTreeTest.java
@@ -1,12 +1,14 @@
 package demo;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Iterator;
 import org.junit.Test;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.tree.ClassNode;
 import org.objectweb.asm.tree.MethodNode;
+import org.objectweb.asm.util.TraceClassVisitor;
 
 public class AsmTreeTest implements Opcodes {
 
@@ -28,5 +30,8 @@ public class AsmTreeTest implements Opcodes {
     for (MethodNode m : cn.methods) {
       System.out.println(m.name + " " + m.desc);
     }
+
+    TraceClassVisitor p = new TraceClassVisitor(new PrintWriter(System.out));
+    cn.accept(p);
   }
 }
